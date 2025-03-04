c901c08e4ef99585b1d441a5add86d7d3664b9ba
niketajain123 <niketajain236@gmail.com>
2025-03-04 13:24:51 +0530

    [Niketa Jain]Add. uc2 Calculate daily wage added.

--- employeeWages.js
+++ employeeWages.js
@@ -1,2 +1,22 @@
+//uc1
 const isPresent = Math.random() < 0.5 ? "Absent" : "Present";
+//uc2
+const getDailyWage = () => {
+  let empCheck = Math.floor(Math.random() * 3);
+  let empHours = 0;
+  switch (empCheck) {
+    case 1:
+      empHours = 4;
+      break;
+    case 2:
+      empHours = 8;
+      break;
+    default:
+      empHours = 0;
+  }
+  return empHours * 20;
+};
+//uc1
 console.log(isPresent);
+// uc2
+console.log(getDailyWage());
